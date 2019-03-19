07cfad23101051a295719327a0e9862676be7b04
Hatice <hnagba@gmail.com>
2019-03-19 17:23:37 +0300

    #Script for camera to track player added

---
+++ DescendingCities/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float zoom;
+    public float playerHeight = 2f;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        transform.position = target.position - offset * zoom;
+        transform.LookAt(target.position + Vector3.up * playerHeight);
+
+    }
+}
