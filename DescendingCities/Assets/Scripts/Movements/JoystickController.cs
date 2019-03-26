254012d6f912426a2d317c9cff480b2f2f1a8c30
Hatice <hnagba@gmail.com>
2019-03-26 16:13:55 +0300

    Joystick and Joystick scripsts added

---
+++ DescendingCities/Assets/Scripts/Movements/JoystickController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class JoystickController : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    private Image backgroundImage;
+    private Image joystickImage;
+    private Vector3 inputVector;
+
+    public void Start()
+    {
+        backgroundImage = GetComponent<Image>();
+        joystickImage = transform.GetChild(0).GetComponent<Image>();
+    }
+
+    public virtual void OnDrag(PointerEventData pointerEventData)
+    {
+        Vector2 position;
+
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(backgroundImage.rectTransform
+                                                                   , pointerEventData.position
+                                                                   , pointerEventData.pressEventCamera
+                                                                   , out position))
+        {
+            
+            position.x = (position.x / backgroundImage.rectTransform.sizeDelta.x);
+            position.y = (position.y / backgroundImage.rectTransform.sizeDelta.y);
+            
+            inputVector = new Vector3(position.x*2, 0, position.y*2);
+
+
+            if (inputVector.magnitude > 1.0f)
+            {
+                inputVector = inputVector.normalized;
+            }
+            
+            MoveJoystickPointer();
+        }
+    }
+
+    public virtual void OnPointerDown(PointerEventData pointerEventData)
+    {
+        OnDrag(pointerEventData);
+      
+    }
+    public virtual void OnPointerUp(PointerEventData pointerEventData)
+    {
+          inputVector = Vector3.zero;
+          joystickImage.rectTransform.anchoredPosition = Vector3.zero;
+    }
+ 
+
+    private void MoveJoystickPointer()
+    {
+        joystickImage.rectTransform.anchoredPosition =
+            new Vector3(inputVector.x * (backgroundImage.rectTransform.sizeDelta.x / 3),
+            inputVector.z * (backgroundImage.rectTransform.sizeDelta.y / 3));
+    }
+
+    public float Horizantal()
+    {
+        if (inputVector.x != 0)
+            return inputVector.x;
+        else
+            return Input.GetAxis("Horizantal");
+    }
+    public float Vertical()
+    {
+        if (inputVector.z != 0)
+            return inputVector.z;
+        else
+            return Input.GetAxis("Vertical");
+    }
+}
---
+++ DescendingCities/Assets/Scripts/Movements/JoystickSize.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JoystickSize : MonoBehaviour
+{
+     private int sizeBackground;
+     private int sizePointer;
+     private Image backgroundImage;
+     private Image joystickImage;
+
+    void Start()
+    {
+        
+        backgroundImage = GetComponent<Image>();
+        joystickImage = transform.GetChild(0).GetComponent<Image>();
+
+
+    }
+    private void Update()
+    {
+        sizeBackground = Screen.width / 6;
+        sizePointer = Screen.width / 15;
+        backgroundImage.rectTransform.sizeDelta = new Vector2(sizeBackground, sizeBackground);
+        joystickImage.rectTransform.sizeDelta = new Vector2(sizePointer, sizePointer);
+
+        backgroundImage.rectTransform.position = new Vector2(sizeBackground / 2, sizeBackground / 2);
+    }
+
+
+}
