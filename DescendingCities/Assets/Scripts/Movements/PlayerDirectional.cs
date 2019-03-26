62577fa2c5ef14a0f7b07a41ab8ee8424f850984
Hatice <hnagba@gmail.com>
2019-03-26 16:16:05 +0300

    Movement synchronized with Joystick

--- DescendingCities/Assets/Scripts/Movements/PlayerDirectional.cs
+++ DescendingCities/Assets/Scripts/Movements/PlayerDirectional.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 
 public class PlayerDirectional : MonoBehaviour {
     public GameObject playerCharacterObject;
-    public int speed = 0;
+    public float speed = 3.0f;
     private float _speed;
     public bool died = false;
-    
+    public JoystickController joystick;
+
     // Use this for initialization
     void Start () {
 		
@@ -22,7 +23,9 @@ public class PlayerDirectional : MonoBehaviour {
 
     private void Move()
     {
-        Vector3 direction = Vector3.zero;
+        /* Direction now comes from Joystick
+         * 
+         * Vector3 direction = Vector3.zero;
 
         if (Input.GetKey(KeyCode.W))
         {
@@ -41,12 +44,22 @@ public class PlayerDirectional : MonoBehaviour {
         else if (Input.GetKey(KeyCode.D))
         {
             direction.x = 1;
-        }
+        }*/
 
+        Vector3 direction = MoveVector();
         transform.LookAt(this.transform.position + direction);
         if (direction != Vector3.zero)
         {
             this.transform.position += this.transform.forward * speed*Time.deltaTime;
         }
+    }
+
+    private Vector3 MoveVector()
+    {
+        Vector3 direction = new Vector3(joystick.Horizantal(), 0, joystick.Vertical());
+        if (direction.magnitude > 1)
+            direction.Normalize();
+
+        return direction;
     }
 }
