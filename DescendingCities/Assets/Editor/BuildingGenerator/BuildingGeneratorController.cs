a88e6415f0370510c045d8f6361914dadb22c553
ThomasBreijer <skylamark@gmail.com>
2019-04-03 17:35:15 +0200

    # Start of Building Generator

---
+++ DescendingCities/Assets/Editor/BuildingGenerator/BuildingGeneratorController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BuildingGenerator))]
+public class BuildingGeneratorController : Editor
+{
+    public bool showHouses;
+
+    public override void OnInspectorGUI()
+    {
+        BuildingGenerator buildingGenerator = (BuildingGenerator)target;
+        EditorGUILayout.LabelField("Cubes Collected:", buildingGenerator.cubeHouses.Length.ToString());
+
+        if (GUILayout.Button("Collect CubeHouses"))
+        {
+            buildingGenerator.CollectAllCubes();
+        }
+        if (GUILayout.Button("Generate Houses"))
+        {
+            buildingGenerator.ConvertToBuilding();
+        }
+
+        if (showHouses)
+        {
+            if (GUILayout.Button("Show Cubes"))
+            {
+                showHouses = false;
+            }
+        } else {
+            if (GUILayout.Button("Show Houses"))
+            {
+                showHouses = true;
+            }
+        }
+
+        //base.OnInspectorGUI();
+    }
+
+
+}
