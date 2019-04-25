3849b8c8a5eb093fe0d2dd2141c665c20a1c0806
ThomasBreijer <skylamark@gmail.com>
2019-04-25 20:04:53 +0200

    # 2 Days of work restored! #

---
+++ DescendingCities/Assets/Teleporter.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+
+public class Teleporter : MonoBehaviour
+{
+    public Transform teleportTargetLocation;
+    private GameObject objToTeleport;
+    public GameObject uIFaderObj;
+    private UI_Fader_Controller uI_Fader;
+    public GameObject buttonTeleport;
+    private Button button;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = buttonTeleport.GetComponent<Button>();
+        uI_Fader = uIFaderObj.GetComponent<UI_Fader_Controller>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        objToTeleport = other.gameObject;
+        buttonTeleport.SetActive(true);
+        button.onClick.RemoveAllListeners();
+        button.onClick.AddListener(() => ActivateTeleport());
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        objToTeleport = null;
+        buttonTeleport.SetActive(false);
+    }
+
+    public void ActivateTeleport()
+    {
+        uI_Fader.FadeIn();
+        Invoke("TeleportObject", 1.5f);
+        objToTeleport.GetComponent<NavMeshAgent>().enabled = false;
+    }
+
+    void TeleportObject()
+    {
+        objToTeleport.transform.position = teleportTargetLocation.transform.position;
+        objToTeleport.transform.rotation = teleportTargetLocation.transform.rotation;
+        objToTeleport.GetComponent<NavMeshAgent>().enabled = true;
+        objToTeleport = null;
+        uI_Fader.FadeOut(0.5f);
+
+    }
+
+}
---
+++ DescendingCities/Assets/UI_Fader_Controller.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Fader_Controller : MonoBehaviour
+{
+    private Animator animator;
+    private Image faderImage;
+
+    // Start is called before the first frame update
+    void Start()    {
+        animator = GetComponent<Animator>();
+        faderImage = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()    {
+        
+    }
+
+    public void FadeIn()
+    {
+        faderImage.material.color = Color.white;
+        animator.SetBool("FadeActive", true);
+    }
+
+    public void FadeOut(float _delay)
+    {
+        Invoke("Fading", _delay);
+        
+    }
+
+    void Fading()
+    {
+        animator.SetBool("FadeActive", false);
+    }
+}
