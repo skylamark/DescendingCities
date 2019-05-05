9c2ed1faafc071751b7cac19e791746ef37d05c3
Hatice <hnagba@gmail.com>
2019-05-05 22:07:56 +0300

    #minimap added with open and close function

---
+++ DescendingCities/Assets/Scripts/Interactions/OpenCloseMiniMap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenCloseMiniMap : MonoBehaviour
+
+{
+
+    public RectTransform plusImage;
+    public RectTransform minimap;
+
+   public void OpenMiniMap()
+    {
+
+        minimap.gameObject.active = true;
+        plusImage.gameObject.active = false;
+    }
+
+    public void CloseMiniMap()
+    {
+        minimap.gameObject.active = false;
+        plusImage.gameObject.active = true;
+    }
+}
