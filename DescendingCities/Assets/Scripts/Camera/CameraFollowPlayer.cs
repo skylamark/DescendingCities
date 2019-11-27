d79f9acd500b312af10ecaeca68925fb96852fbf
ThomasBreijer <skylamark@gmail.com>
2019-11-27 15:56:04 +0100

    + MiniMap Fixed and implemented

---
+++ DescendingCities/Assets/Scripts/Camera/CameraFollowPlayer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+
+    public GameObject player;
+    public float yOffset;
+    private bool hasPlayer = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject _temp;
+        if (player == null)
+        {
+            _temp = GameObject.Find("AVC_Player");
+
+            if (_temp != null)
+            {
+                player = _temp;
+                hasPlayer = true;
+            }
+            else { Debug.Log("Player obj not found."); }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject _temp;
+        if (player == null)
+        {
+            _temp = GameObject.Find("AVC_Player");
+
+            if (_temp != null)
+            {
+                player = _temp;
+                hasPlayer = true;
+            }
+            else { Debug.Log("Player obj not found."); }
+
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (hasPlayer)
+        {
+            transform.position = new Vector3(player.transform.position.x, yOffset, player.transform.position.z);
+        }
+    }
+}
