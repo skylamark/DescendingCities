eb6eec7d3dbff9fb485df82780a8f22266124261
ThomasBreijer <skylamark@gmail.com>
2019-12-11 12:36:49 +0100

    + Cursor Icons, Prefabs: UI location icon, minimap icon, Sprite Shader: render icons in front of everything else, Scipt: IconFaceCamera, - Removed unneeded assets

---
+++ DescendingCities/Assets/Scripts/UI/IconFaceCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconFaceCamera : MonoBehaviour
+{
+    private Transform camera;
+    private SpriteRenderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!camera)
+        { camera = GameObject.FindWithTag("MainCamera").gameObject.transform; }
+        if (!renderer)
+        { renderer = gameObject.GetComponent<SpriteRenderer>(); }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(camera);
+    }
+}
